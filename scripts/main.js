13784a768aaaabbb1000054885a2c5306beba0a1
Jon Huffaker <jonathanhuffaker@gmail.com>
2016-11-16 21:53:54 -0600

    another Kata question

--- scripts/main.js
+++ scripts/main.js
@@ -129,3 +129,28 @@ function filter_list(l) {
 }
 
 // ============
+
+// #6
+
+// You will be given an array of objects representing data about developers who have signed up to attend the next coding meetup that you are organising.
+
+// Given the following input array:
+
+// var list1 = [
+//   { firstName: 'Maria', lastName: 'Y.', country: 'Cyprus', continent: 'Europe', age: 30, language: 'Java' },
+//   { firstName: 'Victoria', lastName: 'T.', country: 'Puerto Rico', continent: 'Americas', age: 70, language: 'Python' },
+// ];
+// write a function that returns the average age of developers (rounded to the nearest integer). In the example above your function should return 50 (number).
+
+// Notes:
+
+// The input array will always be valid and formatted as in the example above.
+// Age is represented by a number which can be any positive integer. 
+
+function getAverageAge(arr) {
+  var totalAge =0;
+  for(var i=0;i<arr.length;i++){
+    totalAge += arr[i].age;
+  }
+  return Math.round(totalAge/arr.length);
+}
