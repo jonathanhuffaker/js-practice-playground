858affbf208922baccf6c738324b11446f4e28af
Jon Huffaker <jonathanhuffaker@gmail.com>
2016-11-20 14:03:21 -0600

    returning min and max of an array

--- scripts/main.js
+++ scripts/main.js
@@ -181,3 +181,9 @@ function f(n){
   }
 };
 // ==============
+// Write a function that returns both the minimum and maximum number of the given list/array.
+
+function minMax(arr){
+  arr.sort(function(a,b) {return a - b});
+  return [arr[0], arr[arr.length - 1]]; // fix me!
+}
