d584fb0a9d454928265057fb6017929421ba73b9
Jon Huffaker <jonathanhuffaker@gmail.com>
2016-11-28 19:37:35 -0600

    taking a string and finding the shortest word

--- scripts/main.js
+++ scripts/main.js
@@ -298,5 +298,20 @@ function towerBuilder(nFloors) {
   }
   return tower;
 }
+// =================
 
+// x Simple, given a string of words, return the length of the shortest word(s).
+
+// String will never be empty and you do not need to account for different data types.
+
+function findShort(s){
+  var arr = s.split(' ');
+  var smallest = arr[0];
+  for (var i = 0; i < arr.length; i++) {
+    if(arr[i].length < smallest.length){
+      smallest = arr[i];
+    }
+  }
+  return smallest.length;
+}
 
