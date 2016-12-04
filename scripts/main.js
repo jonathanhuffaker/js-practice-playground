0490e8c911ae3866c62c7731c6244a23ab082e07
Jon Huffaker <jonathanhuffaker@gmail.com>
2016-12-04 15:59:16 -0600

    Prototype - Dog Bark

--- scripts/main.js
+++ scripts/main.js
@@ -340,4 +340,37 @@ function count(head, data) {
   return (head.data === data ? 1 : 0) + count(head.next, data)
 }
 
+// ==================
+// Make them bark
+
+// You have been hired by a dogbreeder to write a program to keep record of his dogs.
+
+// You've already made a constructor Dog, so no one has to hardcode every puppy.
+
+// The work seems to be done. It's high time to collect the payment.
+
+// ..hold on! The dogbreeder says he wont pay you, until he can make every dog object .bark(). 
+// Even the ones already done with your constructor. "Every dog barks" he says. 
+// He also refuses to rewrite them, lazy as he is.
+
+// You can't even count how much objects that bastard client of yours already made. 
+// He has a lot of dogs, and none of them can .bark().
+
+// Can you solve this problem, or will you let this client outsmart you for good?
+
+// Practical info:
+
+// The .bark() method of a dog should return the string 'Woof!'.
+// The contructor you made (it is preloaded) looks like this:
+
+function Dog(name, breed, gender, age){
+  this.name = name;
+  this.breed = breed;
+  this.gender = gender;
+  this.age = age;
+}
+
+Dog.prototype.bark = function(){
+  return 'Woof!'
+}
 
