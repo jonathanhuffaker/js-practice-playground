c28a9e57dc92b821886856fa365f62211efcd4db
Jon Huffaker <jonathanhuffaker@gmail.com>
2016-12-11 10:45:24 -0600

    best practices answer

--- scripts/main.js
+++ scripts/main.js
@@ -374,3 +374,20 @@ Dog.prototype.bark = function(){
   return 'Woof!'
 }
 
+
+// =======================
+// You are given two arrays a1 and a2 of strings. Each string is composed with letters from a to z. 
+// Let x be any string in the first array and y be any string in the second array.
+
+// Find max(abs(length(x) − length(y)))
+
+// If a1 or a2 are empty return -1 in each language except in Haskell where you will return Nothing.
+
+
+function mxdiflg(a1, a2) {
+  if (a1.length === 0 || a2.length === 0) return -1
+  let l1 = a1.map(str => str.length)
+  let l2 = a2.map(str => str.length)
+  return Math.max(Math.max(...l1) - Math.min(...l2), Math.max(...l2) - Math.min(...l1))
+}
+
